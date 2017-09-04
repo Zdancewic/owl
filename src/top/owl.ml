0c18b1d85992a28a278148e0fea372eda1cc8f60
Steve Zdancewic <stevez@cis.upenn.edu>
2017-09-04 14:40:24 -0400

    better build

---
+++ src/top/owl.ml
@@ -0,0 +1,26 @@
+(*
+ * uTop_start.ml
+ * -------------
+ * Copyright : (c) 2012, Jeremie Dimino <jeremie@dimino.org>
+ * Licence   : BSD3
+ *
+ * This file is a part of utop.
+ *)
+
+(*
+ * owl.ml
+ * ----------------
+ * Copyright : (c) 2015, Steve Zdancewic <steve.zdancewic@gmail.org>
+ * Licence   : BSD3
+ *
+ * This file is a part of owl.
+ *)
+
+(* Changes to the pervasives go here ---------------------------------------- *)
+let print = print_endline
+
+
+
+(*  ------------------------------------------------------------------------- *)
+
+let () = UTop_main.main ()
